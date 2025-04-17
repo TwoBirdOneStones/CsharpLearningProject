4267e6903903fd3591f3c6370fe61431a4f88d47
TwoBirdOneStones <dsvenice98@gmail.com>
2025-04-17 13:03:08 -0400

    change

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 namespace App{
     public static class Program{
         public static void Main(string[] args)
